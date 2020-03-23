c2d3136cfdb66d36f477a0ae36d2483aee18205c
Scott Strool <Scott.Strool@snyk.io>
2020-03-23 09:58:02 -0400

    monday save

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const request = require('request')
 const geocode = require('./lib/geocode.js')
 const forecast = require('./lib/forecast.js')
+// const db = require('./lib/db.js')
 
+const location = process.argv[2]
+if (!location) {
+    return console.log('Location Missing')
+}
 
-geocode('Boston', (error, data) => {
-    if (error) { console.log('Error', error) }
-    console.log('Data', data)
+console.log(`Getting weather for ${location}...`)
+
+geocode(location, (error, geoData) => {
+    if (error) { return console.log('Error', error) }
+    console.log('Data', geoData.location)
+    forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+        if (error) { return console.log('Error', error) }
+        { console.log('Weather:', forecastData) }
+    })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    if (error) { console.log('Error', error) }
-    console.log('Data', data)
-})
